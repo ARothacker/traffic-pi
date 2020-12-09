a7a7de6045c95a73e82029200bf7124967355474
ARothacker <mail@arothacker.de>
2020-12-10 00:31:16 +0100

    add Simple LED Console App

---
+++ simple-led-console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ARWebApps.Learning.TrafficPi.SimpleLedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LED!");
+        }
+    }
+}
